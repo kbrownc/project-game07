b4f8a7c10da935ccd16801b975f7f32034db17f8
Kim Brown <kbrownc341@gmail.com>
2022-06-13 09:00:37 -0400

    Added get and post

--- src/App.js
+++ src/App.js
@@ -2,23 +2,82 @@ import React, { useState, useEffect } from 'react';
 import './App.css';
 
 function App() {
-  const [customers, setCustomers] = useState([]);
+  const [games, setGames] = useState([]);
+  const [gameName, setGameName] = useState('');
+  const [type, setType] = useState('');
+  const [url, setUrl] = useState('');
+  const [error, setError] = useState(null);
+  const published = new Date().toDateString();
+
+  const addGame = (e) => {
+    e.preventDefault();
+    const game = {gameName, type, url, published};
+    console.log('client POST', game);
+    const options = {
+      method: 'POST',
+      headers: {'Content-Type': 'application/json'},
+      body: JSON.stringify(game)
+    };
+    fetch('/api', options)
+    .then(res => res.json())
+    .then(msg => console.log('response', msg));
+  };
 
   useEffect(() => {
-    fetch('/api/customers')
-      .then(res => res.json())
-      .then(customers => setCustomers(customers));
-    console.log('Customers fetched...', customers);
+    fetch('/api/games')
+      .then(res => {
+        if (!res.ok) {
+          throw Error('could not fetch data for api/games');
+        }
+        return res.json();
+      })
+      .then(games => {
+        console.log('client GET',games);
+        setGames(games);
+        setError(null);
+      })
+      .catch(err => {
+        setError(err.message)        
+      }) 
   }, []);
 
   return (
     <div className="App">
       <header className="App-header">
-        <h2>Customers</h2>
+        <h2>Kim Game Inventory</h2>
+        {error && <div>{error}</div>}
+        <form onSubmit={addGame}>
+          <label>Game Name:</label>
+          <input 
+            type="text"
+            required
+            value={gameName}
+            onChange={(e) => setGameName(e.target.value)}
+          />
+          <label>Game Type:</label>
+          <input 
+            type="text"
+            required
+            value={type}
+            onChange={(e) => setType(e.target.value)}
+          />
+          <label>Game URL:</label>
+          <input 
+            type="text"
+            required
+            value={url}
+            onChange={(e) => setUrl(e.target.value)}
+          />
+          <p></p>
+          <button>Add Game</button>
+        </form>
+        <p>{gameName}</p>
+        <p>{type}</p>
+        <p>{url}</p>
         <ul>
-          {customers.map(customer => (
-            <li key={customer.id}>
-              {customer.firstName} {customer.lastName}
+          {games.map(game => (
+            <li key={game._id}>
+              {game.gameName} {game.type} {game.url} {game.published}
             </li>
           ))}
         </ul>
